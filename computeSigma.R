bb08ae36b44ab62cc7c61d8f6799ac96e225f185
Loïc Maréchal <51817463+loicym@users.noreply.github.com>
2021-04-15 14:46:36 +0200

    test
    test

--- computeSigma.R
+++
@@ -1,8 +0,0 @@
-compute_sigma <- function(Z, Yex, Beta, nEq)                    #compute the covariance matrix of errors as in BLS (1998)
-{
-  errors <- (Yex - t(Z) %*% Beta)                               #get the n*p vector of residuals
-  Errors <- matrix(errors, ncol = nEq, byrow = T)               #reset as matrix to obtain Sigma
-  Sigma <- cov(Errors)
-
-  return(Sigma)
-}
